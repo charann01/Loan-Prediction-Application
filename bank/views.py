817dc97bf71d1cb15f8af18ce335e8e2ebdcfabe
ashwinhprasad <ashwinprasad202@gmail.com>
2022-01-28 19:36:06 +0530

    added comments

--- bank/views.py
+++ bank/views.py
@@ -1,13 +1,25 @@
+# importing the necessary modules
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from .forms import AccountRequestForm
 from django.contrib.auth.decorators import login_required
 
+
+"""
+name: Home View
+desc: This view handles the homepage and the information that is to be
+        displayed there.
+"""
 @login_required(login_url='login')
 def HomeView(request):
     return render(request,'home.html',{})
 
+
+"""
+name: Register Request View
+desc: Banks can request to create an account for them through this endpoint.
+"""
 def BankRegistrationRequestView(request):
     if request.method == "POST":
         form = AccountRequestForm(request.POST,request.FILES)
@@ -16,6 +28,11 @@ def BankRegistrationRequestView(request):
     form = AccountRequestForm()
     return render(request,'bank/register.html',{'form':form})
 
+
+"""
+name: Login View
+desc: used by banks for authentication
+"""
 def BankLoginView(request):
     if request.method == "POST":
         bank = authenticate(request,email=request.POST['email'],password=request.POST['password'])
@@ -28,6 +45,11 @@ def BankLoginView(request):
             return redirect('login')
     return render(request,'bank/login.html',{})
 
+
+"""
+name: Logout View
+desc: Sign out of their account
+"""
 def LogoutView(request):
     logout(request)
     return redirect('login')
